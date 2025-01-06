e2c5fdfd33dbff1b432a5c16660a2bdb7dd73be3
adigozal <adigozal.hasanov@gmail.com>
2025-01-06 13:06:07 +0400

    domain name

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,11 @@ FROM python:3.9
 
 WORKDIR /app
 
-# Gereksinimleri yükleyin
 COPY requirements.txt ./
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 
-# Uygulama dosyalarını kopyalayın
 COPY . .
 
-# Portu expose edin
 EXPOSE 8000
 
-# Django sunucusunu başlatın
-CMD [ "python3", "manage.py", "runserver", "0.0.0.0:8000" ]
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "myproject.wsgi:application"]
--- nginx/nginx.conf.d
+++ nginx/nginx.conf.d
@@ -1,24 +1,31 @@
-upstream hello_django {
-    server 127.0.0.1:8000;  # Django uygulamasının çalıştığı yerel IP ve port
+server {
+    listen 80;
+    server_name www.campmen.az;
+
+    # Redirect HTTP to HTTPS
+    return 301 https://$host$request_uri;
 }
 
 server {
-    listen 80;
-    server_name www.campmen.az campmen.az;  # Domain adınızı belirtin
+    listen 443 ssl;
+    server_name www.campmen.az;
+
+    ssl_certificate /etc/nginx/ssl/fullchain.pem;  # SSL sertifikası
+    ssl_certificate_key /etc/nginx/ssl/privkey.pem;  # SSL özel anahtarı
 
     location / {
-        proxy_pass http://hello_django;  # Django uygulamanız
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_pass http://web:8000;  # Django'nun konteynerine yönlendirme
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
-        proxy_redirect off;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
-    
+
     location /static/ {
-        alias /code/static/;  # Statik dosyalarınızın yolu
+        alias /code/static/;
     }
 
     location /media/ {
-        alias /code/media/;  # Medya dosyalarınızın yolu
+        alias /code/media/;
     }
 }
